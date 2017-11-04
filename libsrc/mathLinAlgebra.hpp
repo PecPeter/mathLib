0cbb31e0a5d22c66d66a833cc6f347182f8d5282
PecPeter <peter@peter-arch.localdomain>
2017-11-04 23:55:27 +0000

    Added cVector3 lin alg functions, and moved functions from vector to lin alg file

--- libsrc/mathLinAlgebra.hpp
+++ libsrc/mathLinAlgebra.hpp
@@ -26,4 +26,31 @@ cMatrix rotnTransform (const double rotnRad);
 bool lineIntersection (const cVector2& p1, const cVector2& d1,
 		const cVector2& p2, const cVector2& d2, cVector2& point);
 
+// 2D vector linear algebra functions
+double vSqMagnitude (const cVector2& v1);
+double vMagnitude (const cVector2& v1);
+double vDotProd (const cVector2& v1, const cVector2& v2);
+double vAngleRad (const cVector2& v1, const cVector2& v2);
+double vAngleDeg (const cVector2& v1, const cVector2& v2);
+double vScalProj (const cVector2& projVec, const cVector2& projAxis);
+cVector2 vUnitVector (const cVector2& v1);
+cVector2 vAbsolute (const cVector2& v1);
+cVector2 vNormalR (const cVector2& v1);
+cVector2 vNormalL (const cVector2& v1);
+cVector2 vVecProj (const cVector2& projVec, const cVector2& projAxis);
+cVector2 vRotate (const cVector2& v1, const double& rotnRad);
+cVector2 vMirror (const cVector2& v1, const cVector2& vMirror);
+cVector3 vCrossProd (const cVector2& v1, const cVector2& v2);
+
+cVector2 intersectionLineLine (const cVector2& pt1, const cVector2& lineDir1,
+		const cVector2& pt2, const cVector2& lineDir2);
+double distPtToLine (const cVector2& pt, const cVector2& lineDir,
+		const cVector2& ptLine);
+
+// 3D vector linear algebra functions
+double vSqMagnitude (const cVector3& v1);
+double vMagnitude (const cVector3& v1);
+cVector3 vUnitVector (const cVector3& v1);
+cVector3 vCrossProd (const cVector3& v1, const cVector3& v2);
+
 #endif
--- libsrc/mathVector.hpp
+++ libsrc/mathVector.hpp
@@ -27,6 +27,7 @@ class cVector3 : public cMatrix {
 	public:
 		cVector3 (void);
 		cVector3 (double x, double y, double z);
+		cVector3 (const cVector2& vector);
 		cVector3 (const cVector3& vector);
 		cVector3 (const cMatrix& matrix);
 
@@ -43,6 +44,8 @@ class cVector4 : public cMatrix {
 	public:
 		cVector4 (void);
 		cVector4 (double w, double x, double y, double z);
+		cVector4 (const cVector2& vector);
+		cVector4 (const cVector3& vector);
 		cVector4 (const cVector4& vector);
 		cVector4 (const cMatrix& matrix);
 
@@ -59,24 +62,5 @@ class cVector4 : public cMatrix {
 const cVector2 zeroVector2(0.0,0.0);
 const cVector3 zeroVector3(0.0,0.0,0.0);
 const cVector4 zeroVector4(0.0,0.0,0.0,0.0);
-
-double vSqMagnitude (const cVector2& v1);
-double vMagnitude (const cVector2& v1);
-double vDotProd (const cVector2& v1, const cVector2& v2);
-double vAngleRad (const cVector2& v1, const cVector2& v2);
-double vAngleDeg (const cVector2& v1, const cVector2& v2);
-double vScalProj (const cVector2& projVec, const cVector2& projAxis);
-cVector2 vUnitVector (const cVector2& v1);
-cVector2 vAbsolute (const cVector2& v1);
-cVector2 vNormalR (const cVector2& v1);
-cVector2 vNormalL (const cVector2& v1);
-cVector2 vVecProj (const cVector2& projVec, const cVector2& projAxis);
-cVector2 vRotate (const cVector2& v1, const double& rotnRad);
-cVector2 vMirror (const cVector2& v1, const cVector2& vMirror);
-
-cVector2 intersectionLineLine (const cVector2& pt1, const cVector2& lineDir1,
-		const cVector2& pt2, const cVector2& lineDir2);
-double distPtToLine (const cVector2& pt, const cVector2& lineDir,
-		const cVector2& ptLine);
 
 #endif
